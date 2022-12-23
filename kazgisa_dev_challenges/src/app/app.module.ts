f8ff83e8cc8055959b88509079eff5c660711353
Anna Navoznova <anna.navoznova@gmail.com>
2022-12-23 17:45:36 +0800

    feat: add tree

--- kazgisa_dev_challenges/src/app/app.module.ts
+++ kazgisa_dev_challenges/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SectionsComponent } from './sections/sections.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SectionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ kazgisa_dev_challenges/src/app/sections/sections.component.ts
@@ -0,0 +1,47 @@
+
+  import {NestedTreeControl} from '@angular/cdk/tree';
+  import {Component} from '@angular/core';
+  import {MatTreeNestedDataSource} from '@angular/material/tree';
+
+  interface FoodNode {
+    name: string;
+    children?: FoodNode[];
+  }
+  
+  const TREE_DATA: FoodNode[] = [
+    {
+      name: 'Fruit',
+      children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
+    },
+    {
+      name: 'Vegetables',
+      children: [
+        {
+          name: 'Green',
+          children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
+        },
+        {
+          name: 'Orange',
+          children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
+        },
+      ],
+    },
+  ];
+  
+
+@Component({
+  selector: 'app-sections',
+  templateUrl: './sections.component.html',
+  styleUrls: ['./sections.component.css']
+})
+
+export class SectionsComponent {
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+}
